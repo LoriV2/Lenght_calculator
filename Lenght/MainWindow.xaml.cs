4e1fea392ba2a2497b316ff59574a5bec07bbb9d
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-03-22 15:20:51 +0000

    Dodaj pliki projektów.

---
+++ Lenght/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lenght
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+       
+        }
+        string withoutspace;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = "Lenght of string:  " + TextBox1.Text.Length;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            withoutspace = string.Concat(TextBox1.Text.Where(c => !Char.IsWhiteSpace(c)));
+            Label1.Content = "Lenght of string:  " + withoutspace.Length;
+        }
+    }
+}
